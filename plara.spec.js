972449a4a59ef89b1fa013b9f9e1fd1803412934
davyengone <engonenzedavy@yahoo.com>
2016-03-07 06:04:31 +0100

    tests are important right???

---
+++ plara.spec.js
@@ -0,0 +1,30 @@
+describe('Unit testing fo the function', ()=>{
+
+  describe('Double', ()=>{
+
+  });
+
+  describe('Sum', ()=>{
+
+  });
+
+  describe('Map', ()=>{
+
+  });
+
+  describe('Map', ()=>{
+
+  });
+
+  describe('Reduce', ()=>{
+
+  });
+
+  describe('Extends', ()=>{
+
+  });
+
+  describe('', ()=>{
+
+  });
+});
